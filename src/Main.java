a2d2b58caee4439ad130fba215c817de2bba7159
shravnisingh <shravanisingh22@gmail.com>
2024-12-13 00:39:32 +0530

    UC-02

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,16 @@
 public class Main {
     public static void main(String[] args) {
-        BinarySearchTree bst = new BinarySearchTree();
-        bst.add(56);
-        bst.add(30);
-        bst.add(70);
-        System.out.println("In-order Traversal of BST:");
-        bst.inorderTraversal();
+        BinarySearchTree tree = new BinarySearchTree();
+
+        int[] elements = {56, 30, 70, 22, 40, 60, 95, 3, 16, 65, 63, 67};
+
+        for (int element : elements) {
+            tree.add(element);
+        }
+
+        System.out.println("Inorder Traversal:");
+        tree.inorderTraversal();
+
+        System.out.println("\nSize of the tree: " + tree.size());
     }
 }
